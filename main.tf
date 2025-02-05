883b8d915d1509d3fd73358ba2cdac0a00b3338f
havjay98 <jamesphavercroft@hotmail.com>
2025-02-05 13:05:21 +0000

    web server

--- main.tf
+++ main.tf
@@ -1,9 +1,47 @@
-provider "aws" {
-    region = "us-east-1"
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.85.0"
+    }
+  }
 }
 
-resource "aws_ec2_instance" "example" {
-    ami = "ami-0c614dee691cbbf37"
-    instance_type = "t2.micro"
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0c614dee691cbbf37"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   
+
+
+  /*user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, world" > index.html
+                nohup bussybox httpd -f -p ${var.server_port} &
+                EOF
+    */
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
