f31cf23efa954a68bc9b647835512e80308e01f7
havjay98 <jamesphavercroft@hotmail.com>
2025-02-12 10:58:47 +0000

    web app commit

--- web_app.tf
+++ web_app.tf
@@ -172,3 +172,15 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
